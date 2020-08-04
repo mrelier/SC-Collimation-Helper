5a51d4588402fbc1a00eb01eb80f7259e09ae7f4
Relier <martial.relier@gmail.com>
2020-08-04 23:24:11 +0200

    interface

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const path = require("path");
 function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
+    width: 400,
     height: 600,
     transparent: true,
     frame: false,
